0b43946f66a8932cf73f24798b6078cdcd669cb7
fuzi <fzein.1695@gmail.com>
2024-10-31 09:54:06 +0700

    fix: crud region using thymeleaf

--- src/main/java/com/sibkm/clientapp/controller/RegionController.java
+++ src/main/java/com/sibkm/clientapp/controller/RegionController.java
@@ -5,8 +5,11 @@ import com.sibkm.clientapp.service.RegionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +25,12 @@ public class RegionController {
     return "region/index";
   }
 
+  @GetMapping("/{id}")
+  public String getById(@PathVariable Integer id, Model model) {
+    model.addAttribute("region", regionService.getById(id));
+    return "region/detail";
+  }
+
   @GetMapping("/create")
   public String createView(Region region, Model model) {
     model.addAttribute("region", new Region());
@@ -31,6 +40,28 @@ public class RegionController {
   @PostMapping
   public String create(Region region) {
     regionService.create(region);
+    return "redirect:/region";
+  }
+
+  @GetMapping("/update/{id}")
+  public String updateView(
+    @PathVariable Integer id,
+    Region region,
+    Model model
+  ) {
+    model.addAttribute("region", regionService.getById(id));
+    return "region/update-form";
+  }
+
+  @PutMapping("/{id}")
+  public String update(@PathVariable Integer id, Region region) {
+    regionService.update(id, region);
+    return "redirect:/region";
+  }
+
+  @DeleteMapping("/{id}")
+  public String delete(@PathVariable Integer id) {
+    regionService.delete(id);
     return "redirect:/region";
   }
 }
--- src/main/java/com/sibkm/clientapp/service/RegionService.java
+++ src/main/java/com/sibkm/clientapp/service/RegionService.java
@@ -2,7 +2,9 @@ package com.sibkm.clientapp.service;
 
 import com.sibkm.clientapp.entity.Region;
 import java.util.List;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -10,15 +12,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@AllArgsConstructor
+@Slf4j
 public class RegionService {
 
+  @Value("${server.base.url}/region")
+  private String url;
+
+  @Autowired
   private RestTemplate restTemplate;
 
   public List<Region> getAll() {
     return restTemplate
       .exchange(
-        "http://localhost:9000/region",
+        url,
         HttpMethod.GET,
         null,
         new ParameterizedTypeReference<List<Region>>() {}
@@ -26,14 +32,35 @@ public class RegionService {
       .getBody();
   }
 
+  public Region getById(Integer id) {
+    log.info("endpoint serverapp = {}", url.concat("/" + id));
+
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.GET, null, Region.class)
+      .getBody();
+  }
+
   public Region create(Region region) {
     return restTemplate
       .exchange(
-        "http://localhost:9000/region",
+        url,
         HttpMethod.POST,
         new HttpEntity<Region>(region),
         new ParameterizedTypeReference<Region>() {}
       )
+      .getBody();
+  }
+
+  public Region update(Integer id, Region region) {
+    HttpEntity<Region> request = new HttpEntity<Region>(region);
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.PUT, request, Region.class)
+      .getBody();
+  }
+
+  public Region delete(Integer id) {
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.DELETE, null, Region.class)
       .getBody();
   }
 }
