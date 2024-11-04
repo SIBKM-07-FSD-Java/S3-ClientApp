50494042a124bd7e6a483bc544cf4f4abf2d332d
fuzi <fzein.1695@gmail.com>
2024-11-04 10:01:56 +0700

    add: crud country using thymeleaf

---
+++ src/main/java/com/sibkm/clientapp/controller/CounrtyController.java
@@ -0,0 +1,95 @@
+package com.sibkm.clientapp.controller;
+
+import com.sibkm.clientapp.entity.Country;
+import com.sibkm.clientapp.model.request.CountryRequest;
+import com.sibkm.clientapp.service.CountryService;
+import com.sibkm.clientapp.service.RegionService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/country")
+public class CounrtyController {
+
+  private CountryService countryService;
+  private RegionService regionService;
+
+  @GetMapping
+  public String getAll(Model model) {
+    model.addAttribute("countries", countryService.getAll());
+    return "country/index";
+  }
+
+  // todo: without dto
+  @GetMapping("/{id}")
+  public String getById(@PathVariable Integer id, Model model) {
+    model.addAttribute("country", countryService.getById(id));
+    return "country/detail";
+  }
+
+  // todo: with dto
+  @GetMapping("/res/{id}")
+  public String getByIdDTO(@PathVariable Integer id, Model model) {
+    model.addAttribute("country", countryService.getByIdDTO(id));
+    return "country/detail";
+  }
+
+  // todo: without dto
+  @GetMapping("/create")
+  public String createView(Country country, Model model) {
+    model.addAttribute("country", new Country());
+    model.addAttribute("regions", regionService.getAll());
+    return "country/create-form";
+  }
+
+  @PostMapping
+  public String create(Country country) {
+    countryService.create(country);
+    return "redirect:/country";
+  }
+
+  // todo: with dto
+  @GetMapping("/create-dto")
+  public String createViewDTO(CountryRequest countryRequest, Model model) {
+    model.addAttribute("country", new CountryRequest());
+    model.addAttribute("regions", regionService.getAll());
+    return "country/create-form_dto";
+  }
+
+  @PostMapping("/dto")
+  public String createDTO(CountryRequest countryRequest) {
+    countryService.createDTO(countryRequest);
+    return "redirect:/country";
+  }
+
+  @GetMapping("/update/{id}")
+  public String updateView(
+    @PathVariable Integer id,
+    Country country,
+    Model model
+  ) {
+    model.addAttribute("country", countryService.getById(id));
+    model.addAttribute("regions", regionService.getAll());
+    return "country/update-form";
+  }
+
+  @PutMapping("/{id}")
+  public String update(@PathVariable Integer id, Country country) {
+    countryService.update(id, country);
+    return "redirect:/country";
+  }
+
+  @DeleteMapping("/{id}")
+  public String delete(@PathVariable Integer id) {
+    countryService.delete(id);
+    return "redirect:/country";
+  }
+}
---
+++ src/main/java/com/sibkm/clientapp/service/CountryService.java
@@ -0,0 +1,96 @@
+package com.sibkm.clientapp.service;
+
+import com.sibkm.clientapp.entity.Country;
+import com.sibkm.clientapp.model.request.CountryRequest;
+import com.sibkm.clientapp.model.response.CountryResponse;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
+public class CountryService {
+
+  @Value("${server.base.url}/country")
+  private String url;
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  public List<Country> getAll() {
+    return restTemplate
+      .exchange(
+        url,
+        HttpMethod.GET,
+        null,
+        new ParameterizedTypeReference<List<Country>>() {}
+      )
+      .getBody();
+  }
+
+  // todo: without dto
+  public Country getById(Integer id) {
+    log.info("endpoint serverapp = {}", url.concat("/" + id));
+
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.GET, null, Country.class)
+      .getBody();
+  }
+
+  // todo: with dto
+  public CountryResponse getByIdDTO(Integer id) {
+    log.info("endpoint serverapp = {}", url.concat("/" + id));
+
+    return restTemplate
+      .exchange(
+        url.concat("/res/" + id),
+        HttpMethod.GET,
+        null,
+        CountryResponse.class
+      )
+      .getBody();
+  }
+
+  // todo: without dto
+  public Country create(Country country) {
+    return restTemplate
+      .exchange(
+        url,
+        HttpMethod.POST,
+        new HttpEntity<Country>(country),
+        new ParameterizedTypeReference<Country>() {}
+      )
+      .getBody();
+  }
+
+  // todo: with dto
+  public Country createDTO(CountryRequest countryRequest) {
+    return restTemplate
+      .exchange(
+        url.concat("/dto-manual"),
+        HttpMethod.POST,
+        new HttpEntity<CountryRequest>(countryRequest),
+        new ParameterizedTypeReference<Country>() {}
+      )
+      .getBody();
+  }
+
+  public Country update(Integer id, Country country) {
+    HttpEntity<Country> request = new HttpEntity<Country>(country);
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.PUT, request, Country.class)
+      .getBody();
+  }
+
+  public Country delete(Integer id) {
+    return restTemplate
+      .exchange(url.concat("/" + id), HttpMethod.DELETE, null, Country.class)
+      .getBody();
+  }
+}
