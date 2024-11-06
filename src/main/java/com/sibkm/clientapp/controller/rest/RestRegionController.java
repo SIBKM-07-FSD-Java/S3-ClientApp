e314e4db04ef1c0e068f37a193aefafc94b093df
fuzi <fzein.1695@gmail.com>
2024-11-06 10:31:46 +0700

    add: crud region using ajax

---
+++ src/main/java/com/sibkm/clientapp/controller/rest/RestRegionController.java
@@ -0,0 +1,47 @@
+package com.sibkm.clientapp.controller.rest;
+
+import com.sibkm.clientapp.entity.Region;
+import com.sibkm.clientapp.service.RegionService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/region")
+public class RestRegionController {
+
+  private RegionService regionService;
+
+  @GetMapping
+  public List<Region> getAll() {
+    return regionService.getAll();
+  }
+
+  @GetMapping("/{id}")
+  public Region getById(@PathVariable Integer id) {
+    return regionService.getById(id);
+  }
+
+  @PostMapping
+  public Region create(@RequestBody Region region) {
+    return regionService.create(region);
+  }
+
+  @PutMapping("/{id}")
+  public Region update(@PathVariable Integer id, @RequestBody Region region) {
+    return regionService.update(id, region);
+  }
+
+  @DeleteMapping("/{id}")
+  public Region delete(@PathVariable Integer id) {
+    return regionService.delete(id);
+  }
+}
--- src/main/resources/static/js/region.js
+++ src/main/resources/static/js/region.js
@@ -1,1 +1,228 @@
-console.log("Region");
+// console.log("Region");
+
+$(document).ready(() => {
+  $("#tb-region").DataTable({
+    ajax: {
+      method: "GET",
+      url: "/api/region",
+      dataSrc: "",
+    },
+    columnDefs: [{ className: "text-center", targets: "_all" }],
+    columns: [
+      { data: "id" },
+      { data: "name" },
+      {
+        data: null,
+        render: (data) => {
+          return /* html */ `
+            <div class="d-flex gap-3 justify-content-center align-items-center">
+              <button
+                type="button"
+                class="btn btn-primary btn-sm"
+                data-bs-toggle="modal"
+                data-bs-target="#detail"
+                onclick="findById(${data.id})"
+              >
+                Detail
+              </button>
+              <button
+                type="button"
+                class="btn btn-warning btn-sm"
+                data-bs-toggle="modal"
+                data-bs-target="#update"
+                onclick="beforeUpdate(${data.id})"
+              >
+                Update
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger btn-sm"
+                onclick="deleteRegion(${data.id})"
+              >
+                Delete
+              </button>
+            </div>
+          `;
+        },
+      },
+    ],
+  });
+});
+
+$("#create-region").click((event) => {
+  event.preventDefault();
+
+  const valueName = $("#create-name").val();
+  // console.log(valueName);
+
+  if (valueName === "" || valueName === null) {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Please fill all field!!!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    $.ajax({
+      method: "POST",
+      url: "/api/region",
+      dataType: "JSON",
+      contentType: "application/json",
+      data: JSON.stringify({
+        name: valueName,
+      }),
+      success: (res) => {
+        // console.log(res);
+        $("#create").modal("hide");
+        $("#tb-region").DataTable().ajax.reload();
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Your Region has been saved",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        $("#create-name").val("");
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+});
+
+function findById(id) {
+  // console.log(id);
+
+  $.ajax({
+    method: "GET",
+    url: `/api/region/${id}`,
+    dataType: "JSON",
+    contentType: "application/json",
+    success: (res) => {
+      // console.log(res);
+      $("#detail-id").val(res.id);
+      $("#detail-name").val(res.name);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  });
+}
+
+function beforeUpdate(id) {
+  $.ajax({
+    method: "GET",
+    url: `/api/region/${id}`,
+    dataType: "JSON",
+    contentType: "application/json",
+    success: (res) => {
+      // console.log(res);
+      $("#update-id").val(res.id);
+      $("#update-name").val(res.name);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  });
+}
+
+$("#update-region").click((event) => {
+  event.preventDefault();
+
+  const valueName = $("#update-name").val();
+  const valueId = $("#update-id").val();
+  const updateData = {
+    name: valueName,
+  };
+  // console.log(valueName);
+
+  if (valueName === "" || valueName === null) {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Please fill all field!!!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    $.ajax({
+      method: "PUT",
+      url: `/api/region/${valueId}`,
+      dataType: "JSON",
+      contentType: "application/json",
+      data: JSON.stringify(updateData),
+      success: (res) => {
+        // console.log(res);
+        $("#update").modal("hide");
+        $("#tb-region").DataTable().ajax.reload();
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Your Region has been saved",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        $("#update-name").val("");
+        $("#update-id").val("");
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+});
+
+function deleteRegion(id) {
+  // console.log(id);
+
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success btn-sm ms-3",
+      cancelButton: "btn btn-danger btn-sm",
+    },
+    buttonsStyling: false,
+  });
+  swalWithBootstrapButtons
+    .fire({
+      title: "Are you sure?",
+      text: "You won't be able to delete this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        $.ajax({
+          method: "DELETE",
+          url: `/api/region/${id}`,
+          dataType: "JSON",
+          contentType: "application/json",
+          success: (res) => {
+            // console.log(res);
+            $("#tb-region").DataTable().ajax.reload();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your Region has been deleted.",
+          icon: "success",
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your Region is safe :)",
+          icon: "error",
+        });
+      }
+    });
+}
