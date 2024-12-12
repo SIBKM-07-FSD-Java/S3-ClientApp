585b731bfb0d44da5149a139d7e523bbdc67fc93
fuzi <fzein.1695@gmail.com>
2024-12-12 10:24:38 +0700

    add: extras on thymeleaf

--- src/main/java/com/sibkm/clientapp/controller/HomeController.java
+++ src/main/java/com/sibkm/clientapp/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.sibkm.clientapp.controller;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
   @GetMapping
-  public String index(Model model) {
-    model.addAttribute("name", "SIBKM");
+  public String index(Model model, Principal principal) {
+    // model.addAttribute("name", "SIBKM");
     model.addAttribute("isActive", "home");
+    principal.getName();
     return "index";
   }
 }
